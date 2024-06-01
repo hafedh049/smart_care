dc0d9b5c106d9f4921f56f42ad76cc54b8e7b44d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-06-01 01:10:42 +0100

    Updates

---
+++ lib/stuff/functions.dart
@@ -0,0 +1,163 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:smart_care/stuff/globals.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void showToast({required String text, Color? color}) {
+  Fluttertoast.showToast(msg: text.replaceAll(RegExp(r'\[.+\] '), ''), backgroundColor: color ?? blue.withOpacity(.3), fontSize: 14, gravity: ToastGravity.TOP, toastLength: Toast.LENGTH_LONG, textColor: white, timeInSecForIosWeb: 3);
+}
+
+Future<void> openDB() async {
+  db = await openDatabase(
+    "database.db",
+    version: 1,
+    onCreate: (Database db, int version) {
+      db.execute("CREATE TABLE SMART_CARE (ID INTEGER PRIMARY KEY , FIRST_TIME INTEGER, THEME_MODE INTEGER);");
+      db.insert("SMART_CARE", <String, dynamic>{"FIRST_TIME": 1, "ID": 1, "THEME_MODE": 0});
+    },
+  );
+}
+
+Future<String> takesFromCameraOrGallery(bool camera) async {
+  try {
+    XFile? image = await ImagePicker().pickImage(source: camera ? ImageSource.camera : ImageSource.gallery, imageQuality: 100, preferredCameraDevice: CameraDevice.front);
+    if (image != null) {
+      return await cropImage(image.path);
+    }
+    return "";
+  } catch (_) {
+    showToast(text: _.toString());
+    return "";
+  }
+}
+
+Future<String> cropImage(String imagePath) async {
+  try {
+    CroppedFile? croppedImage = await ImageCropper().cropImage(sourcePath: imagePath);
+    if (croppedImage != null) {
+      return croppedImage.path;
+    }
+    return "";
+  } catch (_) {
+    showToast(text: _.toString());
+    return "";
+  }
+}
+
+bool isImageUrl(String url) {
+  final List<String> imageExtensions = <String>['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+  final RegExp imageRegex = RegExp('.*(${imageExtensions.join('|')})', caseSensitive: false);
+
+  if (!Uri.parse(url).isAbsolute) {
+    return false;
+  }
+
+  return imageRegex.hasMatch(url);
+}
+
+String formatDateTime(DateTime dateTime, TimeOfDay timeOfDay) {
+  final String day = dateTime.day.toString();
+  final String month = dateTime.month.toString();
+  final String year = dateTime.year.toString();
+  final String hours = timeOfDay.hour.toString().padLeft(2, '0');
+  final String minutes = timeOfDay.minute.toString().padLeft(2, '0');
+  final String amPm = timeOfDay.hour < 12 ? 'AM' : 'PM';
+
+  const List<String> monthNames = <String>['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  final String monthName = monthNames[int.parse(month)];
+
+  return '$day $monthName, $year $hours:$minutes $amPm';
+}
+
+String getTimeFromDate(DateTime date) {
+  final String minutes = date.minute.toString().padLeft(2, '0');
+  final String meridian = date.hour < 12 ? 'AM' : 'PM';
+  final String hours = (date.hour % 12).toString().padLeft(2, '0');
+  return '$hours:$minutes $meridian';
+}
+
+String getDateRepresentation(DateTime date) {
+  return "${showWeekDay(date.weekday)}, ${months[date.month]} ${date.day}";
+}
+
+String showWeekDay(int day) {
+  return weekDayPredictor[DateTime(DateTime.now().year, DateTime.now().month, day).weekday]!;
+}
+
+Future<void> getToken() async {
+  if (userToken.isEmpty) {
+    await FirebaseMessaging.instance.getToken().then((String? value) async {
+      userToken = value!;
+      final DocumentSnapshot<Map<String, dynamic>> reference = await FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).get();
+      if (reference.exists) {
+        await FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).update(<String, String>{'token': userToken});
+      }
+    });
+  }
+}
+
+void sendPushNotificationFCM({required String token, required String username, required String message}) async {
+  try {
+    await post(
+      Uri.parse('https://fcm.googleapis.com/fcm/send'),
+      headers: const <String, String>{'Content-Type': 'application/json', 'Authorization': 'key=AAAATAO2yPs:APA91bHBc_S-v6MHnfTRxz1PD60a_Lh0yY4cB-q4FJlFSKR4To97gAb8bGXECJTKVjWTHo_1fAzSer5ae8CcwL7zK24N45y0VuXWkFN1n0aHapTNCV2DyRYUvXbqG0nu4OsBMvnbXRTf'},
+      body: jsonEncode(
+        <String, dynamic>{
+          'notification': <String, dynamic>{'body': message, 'title': username},
+          'priority': 'high',
+          'data': const <String, dynamic>{'click_action': 'FLUTTER_NOTIFICATION_CLICK', 'id': '1', 'status': "done"},
+          'to': token,
+        },
+      ),
+    );
+  } catch (e) {
+    showToast(text: e.toString());
+  }
+}
+
+Future<void> goTo(Widget place) async {
+  await Get.to(place, transition: animatedTransitions[Random().nextInt(animatedTransitions.length)], duration: 300.ms);
+}
+
+Future<String> getChatResponse(String input) async {
+  final Map<String, String> headers = <String, String>{'Content-Type': 'application/json', 'Authorization': 'Bearer $gpt3ApiKey'};
+  final Map<String, dynamic> body = <String, dynamic>{'prompt': input, 'temperature': .5, 'max_tokens': 128};
+  final response = await post(Uri.parse(gpt3ApiUrl), headers: headers, body: json.encode(body));
+  final data = json.decode(response.body);
+  final String chatResponse = data['choices'][0]['text'];
+  return chatResponse;
+}
+
+String getChatId(List<String> ids) {
+  ids.sort();
+  return '${ids[0]}_${ids[1]}';
+}
+
+Future<List<Appointment>> generateAppointments() async {
+  final QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance.collection("appointments").where(me["role"] == "doctor" ? "doctorID" : "patientID", isEqualTo: me["uid"]).get();
+
+  return snapshot.docs
+      .map(
+        (QueryDocumentSnapshot<Map<String, dynamic>> item) => Appointment(
+          startTime: item.get("appointmentDate").toDate(),
+          endTime: item.get("appointmentDate").toDate().add(const Duration(minutes: 60 /*int.parse(item.get("duration").split(" ")[0])*/)),
+          color: blue,
+          subject: "Appointment",
+          notes: me["role"] == "doctor" ? item["patientName"] : item["doctorName"],
+        ),
+      )
+      .toList();
+}
---
+++ lib/stuff/globals.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../screens/admin/charts/grades.dart';
+import '../screens/admin/charts/hospitals.dart';
+import '../screens/admin/charts/services.dart';
+
+//colors
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color darkBlue = Color(0xff272336);
+const Color transparent = Colors.transparent;
+const Color blue = Color.fromARGB(255, 93, 172, 248);
+const Color red = Color.fromARGB(255, 219, 15, 0);
+const Color green = Colors.lightGreenAccent;
+const Color grey = Colors.grey;
+
+//global variables
+int? firstTime;
+int themeMode = 0;
+String userToken = "";
+const String appTitle = "Smart Care";
+const String aboutUs = "Welcome to our telemedicine platform, built using the Flutter cross-platform framework, designed to provide primary prevention and prompt medical assistance in case of an AES (accident d'explosion du sang). Our platform is not only for patients, but it also caters to healthcare professionals who can monitor their patient's health status in real-time. Our platform's primary objective is to assist people in preventing AES by conducting daily and monthly checkups and sending SMS or notification reminders. In case of an AES event, our platform provides a workflow that patients can follow and get the final results from our laboratory experts. We understand the importance of medical assistance during an AES event; hence, we have provided a direct chat option with doctors. In addition, we have integrated a smart chatbot built on top of ChatGPT 3.5 for faster responses to queries when doctors are unavailable. Our telemedicine platform is fully customizable, with two themes - light and dark, and supports eight languages, making it user-friendly for people from diverse backgrounds. You can also enable gesture and message sounds to personalize your experience.Our platform offers a multitude of benefits for both patients and healthcare professionals. Patients can access medical assistance from the comfort of their homes, while healthcare professionals can monitor their patient's health status and offer timely intervention. We believe that our telemedicine platform can help reduce the incidence of AES and save lives.";
+
+//API-Keys
+const String gpt3ApiKey = "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";
+const String gpt3ApiUrl = 'https://api.openai.com/v1/engines/davinci/completions'; //https://api.openai.com/v1/completions
+const String newsApiKey = "4ab974b0133747658c75513590257f4e";
+
+//User Data
+Map<String, dynamic> me = <String, dynamic>{};
+
+//Database Reference
+Database? db;
+
+//Network Images
+const String chatBot = "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/smart_bot.png?alt=media&token=99ba8285-3b29-489a-a473-a81cc228e3d3";
+const String noUser = 'https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/noUser.png?alt=media&token=476f6c72-ce24-497e-ad28-d4e3e0b3ee6b';
+const String rodeOfAsclepius = "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/virus.jpg?alt=media&token=0bdcf248-dab6-4e77-a7f5-ce8a9667fc19";
+const String doctorRod = "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/WallpaperDog-5497244-min.jpg?alt=media&token=d66c6ae6-8b28-4c58-8d1f-2d41106f44b3";
+const String appIcon = "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/health.png?alt=media&token=5b9a461b-bf46-4dfd-bc83-e8ec9a66f990";
+const String avuRL = "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/av.png?alt=media&token=f6f072a1-c011-4c35-aaa4-946f100ba206&_gl=1*y63jnt*_ga*MTE5NDk5NjQ4MS4xNjgyOTgwODQx*_ga_CW55HF8NVT*MTY4NTc1MTk2OC41Ny4xLjE2ODU3NTIwMDcuMC4wLjA.";
+
+//Data Structures
+final List<Map<String, dynamic>> gradesList = <Map<String, dynamic>>[
+  {
+    'url': "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/specialities%2F1-min.jpg?alt=media&token=e8e0e7eb-e787-4731-8adb-c6d3ae0e5b23",
+    "grade": "Expert métier".tr,
+    "description": "Surgeons and surgicals",
+  },
+  {
+    'url': "https://firebasestorage.googleapis.com/v0/b/smart-care-b4ab6.appspot.com/o/specialities%2F1-min.jpg?alt=media&token=e8e0e7eb-e787-4731-8adb-c6d3ae0e5b23",
+    "grade": "Expert métier".tr,
+    "description": "Surgeons and surgicals",
+  },
+];
+
+final Map<int, dynamic> months = <int, dynamic>{1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"};
+
+final Map<int, String> weekDayPredictor = <int, String>{1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"};
+
+final List<Map<String, dynamic>> healthcareFacilities = <Map<String, dynamic>>[
+  <String, dynamic>{"name": "Centre Hospitalier Universitaire Fattouma Bourguiba de Monastir", "latitude": 35.7611469, "longitude": 10.8125058},
+  <String, dynamic>{"name": "Clinique les Oliviers Monastir", "latitude": 35.7667159, "longitude": 10.8322074},
+  <String, dynamic>{"name": "Clinique El Amen Monastir", "latitude": 35.7713949, "longitude": 10.8206096},
+  <String, dynamic>{"name": "Polyclinique des Palmiers Monastir", "latitude": 35.7604815, "longitude": 10.827065},
+  <String, dynamic>{"name": "Clinique Internationale Monastir", "latitude": 35.768876, "longitude": 10.8319216},
+  <String, dynamic>{"name": "Clinique Al Hikma Monastir", "latitude": 35.7701055, "longitude": 10.8300627},
+  <String, dynamic>{"name": "Clinique Monastir Médical", "latitude": 35.7720048, "longitude": 10.8286933},
+  <String, dynamic>{"name": "Clinique Al Amana Monastir", "latitude": 35.7712026, "longitude": 10.8256544},
+  <String, dynamic>{"name": "Polyclinique Al Azhar Monastir", "latitude": 35.7616056, "longitude": 10.8219744},
+  <String, dynamic>{"name": "Clinique Taoufik Monastir", "latitude": 35.7640652, "longitude": 10.8251672},
+  <String, dynamic>{"name": "Polyclinique de la Rose Monastir", "latitude": 35.7631194, "longitude": 10.8342604},
+  <String, dynamic>{"name": "Polyclinique Essafaa Monastir", "latitude": 35.7627449, "longitude": 10.8324411},
+  <String, dynamic>{"name": "Polyclinique Monastir El Wifak", "latitude": 35.7659385, "longitude": 10.8295595},
+  <String, dynamic>{"name": "Hôpital de Moknine", "latitude": 35.6078568, "longitude": 10.995835},
+  <String, dynamic>{"name": "Hôpital de Zéramdine", "latitude": 35.4122763, "longitude": 10.978685},
+  <String, dynamic>{"name": "Hôpital de Jemmal", "latitude": 35.5655434, "longitude": 10.9052743},
+  <String, dynamic>{"name": "Hôpital de Ksar Hellal", "latitude": 35.670407, "longitude": 10.877977},
+  <String, dynamic>{"name": "Hôpital de Sahline", "latitude": 35.7840403, "longitude": 10.6963433},
+  <String, dynamic>{"name": "Hôpital de Sayada", "latitude": 36.8730225, "longitude": 10.326227},
+  <String, dynamic>{"name": "Faculté de Médecine de Monastir", "latitude": 35.7763282, "longitude": 10.8267896}
+];
+
+final List<Map<String, dynamic>> charts = <Map<String, dynamic>>[
+  <String, dynamic>{"name": 'Hospitals Chart'.tr, "widget": const Hospitals(), "color": const Color.fromARGB(255, 70, 130, 180), "icon": FontAwesomeIcons.chartBar},
+  <String, dynamic>{"name": 'Grades Chart'.tr, "widget": const Grades(), "color": green, "icon": FontAwesomeIcons.chartLine},
+  <String, dynamic>{"name": 'Services Chart'.tr, "widget": const Services(), "color": const Color.fromARGB(255, 255, 204, 0), "icon": FontAwesomeIcons.chartColumn},
+];
+
+const List<Transition> animatedTransitions = Transition.values;
+
+final Map<String, String? Function(String?)?> fieldsValidator = <String, String? Function(String?)?>{
+  "about": (String? text) {
+    return null;
+  },
+  "email": (String? text) {
+    if (text!.isEmpty) {
+      return 'emailismandatory'.tr;
+    } else if (!text.contains(RegExp(r'^[\w\.-]+@[\w\.-]+\.\w{2,4}$'))) {
+      return 'pleaseverifyyourEmail'.tr;
+    }
+    return null;
+  },
+  "password": (String? text) {
+    if (text!.isEmpty) {
+      return 'passwordshouldnotbeempty'.tr;
+    } else if (!text.contains(RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{6,}$'))) {
+      return 'passwordexpressionshouldbeatleastcharactersmustcontainatleastuppercaseletterlowercaseletterandnumberandcancontainspecialcharacters'.tr;
+    }
+    return null;
+  },
+  "username": (String? text) {
+    if (text!.isEmpty) {
+      return 'namefieldisempty'.tr;
+    } else if (!text.contains(RegExp(r'^[a-zA-Z][\w ]+$'))) {
+      return 'thisfieldmuststartwithanalphabeticcharacter'.tr;
+    }
+    return null;
+  },
+  "id": (String? text) {
+    if (text!.isEmpty) {
+      return 'iDmustnotbeempty'.tr;
+    } else if (!text.contains(RegExp(r'^\d{8}$'))) {
+      return 'wrongformatforIDItmuststartwithandcontainsalphanumericcaracters'.tr;
+    }
+    return null;
+  },
+};
+final List<Map<String, dynamic>> workflow = <Map<String, dynamic>>[
+  <String, dynamic>{
+    'index': 0,
+    "title": "Description de La lésion",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Superficielle", "redirectTo": 1},
+      <String, dynamic>{"content": "Profond", "redirectTo": 1},
+    ],
+  },
+  <String, dynamic>{
+    'index': 1,
+    "title": "Mécanisme",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Piqûre par une aiguille pleine", "redirectTo": 2},
+      <String, dynamic>{"content": "Piqûre par une aiguille creuse", "redirectTo": 2},
+      <String, dynamic>{"content": "Piqûre non précisée", "redirectTo": 2},
+      <String, dynamic>{"content": "Coupure", "redirectTo": 2},
+      <String, dynamic>{"content": "Projection de sang", "redirectTo": 2},
+      <String, dynamic>{"content": "Projection du liquide biologique", "redirectTo": 2},
+    ],
+  },
+  <String, dynamic>{
+    'index': 2,
+    "title": "Le Malade-Source",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Inconnu", "redirectTo": 3},
+      <String, dynamic>{"content": "Externe", "redirectTo": 3},
+      <String, dynamic>{"content": "Hospitalisé", "redirectTo": 3},
+    ],
+  },
+  <String, dynamic>{
+    'index': 3,
+    "title": "Quel est le virus avec lequel le patient source est infecté ?",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "VHB", "redirectTo": 4},
+      <String, dynamic>{"content": "VHC", "redirectTo": 9},
+      <String, dynamic>{"content": "HIV", "redirectTo": 12},
+    ],
+  },
+  <String, dynamic>{
+    'index': 4,
+    "title": "VHB",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Vaccinée immunisée, anticorps anti-hépatite B surface (AntiHBS) positif", "redirectTo": 5},
+      <String, dynamic>{"content": "AC anti HBS > 10 (UI/L)", "redirectTo": 6},
+    ],
+  },
+  <String, dynamic>{'index': 5, "end": "Vous n'êtes pas affecté par un virus tant que vous etes immunisées"},
+  <String, dynamic>{
+    'index': 6,
+    "title": "",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Patient source AgHBS +", "redirectTo": 7},
+      <String, dynamic>{"content": "Patient source AgHBS -", "redirectTo": 8},
+    ],
+  },
+  <String, dynamic>{'index': 7, "end": "Il est recommandé de recevoir une injection d'immunoglobuline dans un délai de 72 heures et prendre une dose de vaccin et consulter un gastro-entérologue"},
+  <String, dynamic>{'index': 8, "end": "Il est recommandé de recevoir une injection de vaccin Dose_0"},
+  <String, dynamic>{
+    'index': 9,
+    "title": "VHC",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Patient Source positive (+) : Virus de l'Hépatite C (VHC) positif ou statut inconnu.", "redirectTo": 11},
+      <String, dynamic>{"content": "Patient Source negative (-)", "redirectTo": 10},
+    ],
+  },
+  <String, dynamic>{'index': 10, "end": "Il est recommandé de maintenir une surveillance médicale, comprenant des analyses de transaminases et des sérologies pour le virus de l'hépatite C (VHC), à effectuer tous les 3 mois"},
+  <String, dynamic>{'index': 11, "end": "Il n'est pas nécessaire de suivre une prophylaxie spécifique dans ce cas"},
+  <String, dynamic>{
+    'index': 12,
+    "title": "HIV",
+    "options": <Map<String, dynamic>>[
+      <String, dynamic>{"content": "Patient source positive : Virus de l'Immunodéficience Humaine (VIH) positif.", "redirectTo": 13},
+      <String, dynamic>{"content": "Patient source positive : Virus de l'Immunodéficience Humaine (VIH) négatif.", "redirectTo": 14},
+      <String, dynamic>{"content": "Patient source positive : Virus de l'Immunodéficience Humaine (VIH) inconnu.", "redirectTo": 15},
+    ],
+  },
+  <String, dynamic>{'index': 13, "end": " commencer une trithérapie dans les 4 heures suivantes et de rester sous surveillance médicale et réaliser des tests de sérologie pour le VIH chaque 3 mois "},
+  <String, dynamic>{'index': 14, "end": "Pas de prophylaxie"},
+  <String, dynamic>{'index': 15, "end": "rester sous surveillance médicale et réaliser des tests de sérologie pour le VIH chaque 3 mois."},
+];
